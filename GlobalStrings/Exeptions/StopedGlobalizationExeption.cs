f03e9a2622746d3fb31e838abdbe6656a51a6fba
LuanRoger <luan.roger.2003@gmail.com>
2021-06-16 09:15:36 -0300

    v0.0.4
    Features:
    - Improved error handling.

---
+++ GlobalStrings/Exeptions/StopedGlobalizationExeption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalStrings.Exeptions
+{
+    [Serializable]
+    class StopedGlobalizationExeption : Exception
+    {
+        public StopedGlobalizationExeption() : base() {}
+        public StopedGlobalizationExeption(string message) : base(message) {}
+        public StopedGlobalizationExeption(string message, Exception inner) {}
+        public StopedGlobalizationExeption(SerializationInfo sInfo, 
+            StreamingContext streamingContext) : base(sInfo, streamingContext) {}
+    }
+}
